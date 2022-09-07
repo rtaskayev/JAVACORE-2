a09bdda2592054fd6e4743db62a0edfe339e2620
rtaskayev <washingtonqwerty@gmail.com>
2022-09-07 22:31:59 +0200

    Streams task2

--- src/com/netology/Main.java
+++ src/com/netology/Main.java
@@ -1,24 +1,45 @@
 package com.netology;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
 
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-
-        List<Integer> result = new ArrayList<>();
-
-        for (Integer x : intList) {
-            if ((x > 0) && (x % 2 == 0)) {
-                result.add(x);
-            }
         }
 
-        result.sort(Comparator.naturalOrder());
+        long stream1 = persons.stream()
+                .filter(x -> x.getAge() >= 18)
+                .count();
+        System.out.println(stream1);
 
-        System.out.println(result);
+        List<String> stream2 = persons.stream()
+                .filter(x -> x.getSex().equals(Sex.MAN))
+                .filter(x -> x.getAge() >= 18 && x.getAge() < 27)
+                .map(x -> x.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(stream2);
+
+        List<Person> stream3 = persons.stream()
+                .filter(x -> x.getEducation().equals(Education.HIGHER))
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> (x.getSex().equals(Sex.MAN) && x.getAge() < 65) || (x.getSex().equals(Sex.WOMAN) && x.getAge() < 60))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println(stream3);
 
     }
 }
